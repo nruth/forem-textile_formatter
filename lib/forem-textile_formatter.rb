a770f3398cc3491c0a58f373a9bdfff36df31971
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-10-12 19:01:37 +0200

    bundler skeleton

---
+++ lib/forem-textile_formatter.rb
@@ -0,0 +1,7 @@
+require "forem-textile_formatter/version"
+
+module Forem
+  module TextileFormatter
+    # Your code goes here...
+  end
+end
---
+++ lib/forem-textile_formatter/version.rb
@@ -0,0 +1,5 @@
+module Forem
+  module TextileFormatter
+    VERSION = "0.0.1"
+  end
+end
