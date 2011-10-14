b8bb0f9bc9de4d480b397e9cc7aabc6a532ee8af
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-10-15 01:56:23 +0200

    fixes loading error

--- lib/forem-textile_formatter.rb
+++ lib/forem-textile_formatter.rb
@@ -1,9 +1,10 @@
 require "forem-textile_formatter/version"
+require 'RedCloth'
 
 module Forem
   module FormattingHelper
     def as_formatted_html(text)
-      RedCloth.new(text).to_html.html_safe
+      ::RedCloth.new(text).to_html.html_safe
     end
   end
 end
